088db8aca805cd442577e585ae99cef713f4086b
deffaccto <uros.spasojevic.94@gmail.com>
2015-03-31 23:15:13 +0200

    implementiran je interfejs u klasi Menjacnica bez zavrsenih metoda

---
+++ Menjacnica/src/valuta/Menjacnica.java
@@ -0,0 +1,27 @@
+package valuta;
+
+import java.util.GregorianCalendar;
+
+import valuta.interfejsi.ValutaInterfejs;
+
+public class Menjacnica implements ValutaInterfejs {
+
+	@Override
+	public void dodavajKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double nadjiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
