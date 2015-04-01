f07efc1a81113959c266ccf5ab90b6f226ac69eb
deffaccto <uros.spasojevic.94@gmail.com>
2015-04-01 23:15:31 +0200

    promena metoda u interfejsu ValutaInterfejs; zavrsene metode u klasi Menjacnica

--- Menjacnica/src/valuta/Menjacnica.java
+++ Menjacnica/src/valuta/Menjacnica.java
@@ -6,22 +6,50 @@ import valuta.interfejsi.ValutaInterfejs;
 
 public class Menjacnica implements ValutaInterfejs {
 
-	@Override
-	public void dodavajKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
+	private Valuta[] nizValuta=new Valuta[20];
+		
 	public void obrisiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < nizValuta.length; i++) {
+			if(nizValuta[i].getDatum()==datum)
+				nizValuta[i]=null;
+		}
 
 	}
 
-	@Override
+	
 	public double nadjiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		Valuta novaValuta=new Valuta();
+	
+		for (int i = 0; i < nizValuta.length; i++) {
+			if(nizValuta[i].getDatum()==datum){
+				novaValuta=nizValuta[i];
+				break;
+			}
+		}
+		
+		return novaValuta.getKupovni();
+	}
+
+
+	
+	public void dodavajKurs(String naziv, String skracenica,
+			double prodajniKurs, double srednjiKurs, double kupovniKurs,
+			GregorianCalendar datum) {
+		
+		Valuta novaValuta=new Valuta();
+		novaValuta.setDatum(datum);
+		novaValuta.setKupovni(kupovniKurs);
+		novaValuta.setNaziv(naziv);
+		novaValuta.setProdajni(prodajniKurs);
+		novaValuta.setSkracenica(skracenica);
+		novaValuta.setSrednji(srednjiKurs);
+		
+		for (int i = 0; i < nizValuta.length; i++) {
+			if(nizValuta[i]!=null)
+				nizValuta[i]=novaValuta;
+		}
+		
 	}
 
 }
